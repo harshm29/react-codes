f7f1e619a65a5747dd8f8557a4658ad0a4b54cae
harshmakavana-sdn <harshmakavana04@gmail.com>
2024-04-26 17:39:55 +0530

    dark and light

--- 08theme-switch/src/App.jsx
+++ 08theme-switch/src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./contexts/Theme";
+import Themebtn from "./components/Themebtn";
+import Card from "./components/Card";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
-      <h1 className="text-center text-3xl">Theme Switch</h1>
-    </>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Themebtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
